27d55c43209def19947d5dc0eedc0fe8c0c4c9c1
Daniel Cabral Correa <daniel.liboos@gmail.com>
2018-08-15 08:27:02 -0300

    alterado

---
+++ src/main/java/poo/Principal.java
@@ -0,0 +1,11 @@
+package poo;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+
+
+
+        System.out.println("bom dia");
+    }
+}
